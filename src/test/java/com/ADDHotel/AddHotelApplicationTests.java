af7ef432b08f4a4ec0efad84f9b4c4bf62a29853
Steven Garcia <Stevgtpayes@gmail.com>
2022-12-25 22:18:32 -0600

    firsti commit

---
+++ src/test/java/com/ADDHotel/AddHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ADDHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
